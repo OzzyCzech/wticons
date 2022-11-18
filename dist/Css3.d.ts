6a924c98587b037dfa8b8677a4b43d73e0b8d754
Roman Ožana <roman@ozana.cz>
2022-11-18 23:00:26 +0100

    Modules

---
+++ dist/Css3.d.ts
@@ -0,0 +1,7 @@
+import { SVGProps } from 'react';
+interface SVGRProps {
+    title?: string;
+    titleId?: string;
+}
+declare const SvgCss3: ({ title, titleId, ...props }: SVGProps<SVGSVGElement> & SVGRProps) => JSX.Element;
+export default SvgCss3;
---
+++ dist/Javascript.d.ts
@@ -0,0 +1,7 @@
+import { SVGProps } from 'react';
+interface SVGRProps {
+    title?: string;
+    titleId?: string;
+}
+declare const SvgJavascript: ({ title, titleId, ...props }: SVGProps<SVGSVGElement> & SVGRProps) => JSX.Element;
+export default SvgJavascript;
