dc16efeb85b0c63970dc3647696bb238ba3bfb3b
Roman Ožana <roman@ozana.cz>
2022-11-22 13:14:00 +0100

    Cypress upgrade

--- dist/Cypress.js
+++ dist/Cypress.js
@@ -16,69 +16,18 @@ var SvgCypress = function (props) { return (React.createElement("svg", __assign(
         strokeLinejoin: 'round',
         strokeMiterlimit: 2,
     }, viewBox: "0 0 256 256" }, props),
-    React.createElement("path", { d: "m141.651 223.943-4.335 10.526c-.861 2.084-2.83 3.495-5.046 3.591-1.464.055-2.94.096-4.431.096-51.024-.027-96.169-36.113-107.355-85.836l-17.369 3.933c6.277 27.89 22.032 53.204 44.365 71.295 22.592 18.296 51.052 28.397 80.167 28.452h.26c1.71 0 3.406-.041 5.101-.109 9.136-.384 17.3-6.127 20.788-14.638l5.661-13.76-17.806-3.55Z", style: {
-            fill: 'url(#cypress_svg__a)',
+    React.createElement("path", { d: "M128 3c69.091 0 125 55.909 125 125 0 69.091-55.909 125-125 125C58.909 253 3 197.091 3 128 3 58.909 58.909 3 128 3Z", style: {
             fillRule: 'nonzero',
         } }),
-    React.createElement("path", { d: "M4.54 161.914A128.528 128.528 0 0 1 0 128.007C0 75.516 32.863 27.52 81.782 8.593l6.414 16.652c-42.108 16.295-70.39 57.589-70.39 102.762 0 9.909 1.313 19.722 3.898 29.178L4.54 161.914Z", style: {
-            fill: 'url(#cypress_svg__b)',
+    React.createElement("path", { d: "M128 3c69.091 0 125 55.909 125 125 0 69.091-55.909 125-125 125C58.909 253 3 197.091 3 128 3 58.909 58.909 3 128 3Z", style: {
+            fill: '#fff',
+        } }),
+    React.createElement("path", { d: "M128 7.386c66.667 0 120.614 53.947 120.614 120.614S194.667 248.614 128 248.614 7.386 194.667 7.386 128 61.333 7.386 128 7.386Z", style: {
+            fill: '#5c5c5e',
             fillRule: 'nonzero',
         } }),
-    React.createElement("path", { d: "M225.693 77.298c-19.037-36.662-56.509-59.44-97.769-59.44-14.619 0-28.828 2.823-42.204 8.374L78.897 9.744C94.446 3.289 110.939 0 127.924 0c47.934 0 91.451 26.451 113.564 69.047l-15.795 8.251Z", style: {
-            fill: 'url(#cypress_svg__c)',
+    React.createElement("path", { d: "M192.035 181.07c-4.386 13.597-10.965 23.684-20.175 31.141-9.211 7.456-21.492 11.403-36.842 12.719l-3.071-20.176c10.088-1.315 17.544-3.508 22.369-7.017 1.754-1.316 5.263-5.263 5.263-5.263L123.175 75.807h30.264l21.052 87.281 22.369-87.281h29.386L192.035 181.07ZM86.333 71.86c7.018 0 13.597.877 18.86 3.07 5.702 2.193 10.965 5.263 16.228 9.649l-12.281 16.667c-3.508-2.632-7.017-4.386-10.087-5.702-3.071-1.316-7.018-1.755-10.527-1.755-14.912 0-22.368 11.404-22.368 34.65 0 11.842 1.754 20.175 5.702 25 3.947 5.263 9.21 7.456 16.666 7.456 3.509 0 7.018-.439 10.088-1.755 3.07-1.315 6.579-3.07 10.965-5.701l12.281 17.543c-10.088 8.334-21.492 12.281-34.649 12.281-10.527 0-19.299-2.193-27.193-6.579-7.457-4.386-13.597-10.965-17.544-19.298-3.948-8.333-6.141-17.982-6.141-29.386 0-10.965 2.193-21.053 6.141-29.386 3.947-8.772 10.087-15.351 17.544-20.175 7.456-3.948 16.228-6.579 26.315-6.579Z", style: {
+            fill: '#fff',
             fillRule: 'nonzero',
-        } }),
-    React.createElement("path", { d: "M89.727 101.597c10.326 0 18.737 5.523 23.058 15.158l.342.754 17.341-5.907-.37-.891c-6.715-16.405-22.182-26.602-40.371-26.602-12.786 0-23.18 4.112-31.769 12.554-8.533 8.388-12.855 18.941-12.855 31.371 0 12.321 4.322 22.819 12.855 31.207 8.589 8.443 18.983 12.554 31.769 12.554 18.189 0 33.656-10.197 40.371-26.588l.37-.891-17.368-5.921-.329.782c-3.87 9.47-12.486 15.13-23.044 15.13-7.193 0-13.265-2.521-18.079-7.483-4.868-5.03-7.33-11.348-7.33-18.776 0-7.483 2.407-13.678 7.33-18.941 4.8-4.988 10.886-7.51 18.079-7.51Z", style: {
-            fill: '#09293f',
-            fillRule: 'nonzero',
-        } }),
-    React.createElement("path", { d: "m198.341 85.726-24.712 62.729-24.877-62.729h-20.35l34.888 85.617-24.822 60.385 17.792 3.55 61.309-149.552h-19.228Z", style: {
-            fill: '#09293f',
-            fillRule: 'nonzero',
-        } }),
-    React.createElement("path", { d: "M241.488 69.047C250.491 86.585 256 107.185 256 128.271c0 49.2-27.671 91.916-68.253 113.343l-8.768-15.548c35.191-18.352 59.227-55.258 59.227-97.795 0-18.459-4.513-35.691-12.513-50.973l15.795-8.251Z", style: {
-            fill: 'url(#cypress_svg__d)',
-            fillRule: 'nonzero',
-        } }),
-    React.createElement("defs", null,
-        React.createElement("linearGradient", { id: "cypress_svg__a", x1: 0, x2: 1, y1: 0, y2: 0, gradientTransform: "rotate(6.696 -1685.226 210.013) scale(127.822)", gradientUnits: "userSpaceOnUse" },
-            React.createElement("stop", { offset: 0, style: {
-                    stopColor: '#315c55',
-                    stopOpacity: 1,
-                } }),
-            React.createElement("stop", { offset: 1, style: {
-                    stopColor: '#09293f',
-                    stopOpacity: 1,
-                } })),
-        React.createElement("linearGradient", { id: "cypress_svg__b", x1: 0, x2: 1, y1: 0, y2: 0, gradientTransform: "rotate(91.947 -.41 48.34) scale(111.58639)", gradientUnits: "userSpaceOnUse" },
-            React.createElement("stop", { offset: 0, style: {
-                    stopColor: '#4fb38d',
-                    stopOpacity: 1,
-                } }),
-            React.createElement("stop", { offset: 1, style: {
-                    stopColor: '#315c55',
-                    stopOpacity: 1,
-                } })),
-        React.createElement("linearGradient", { id: "cypress_svg__c", x1: 0, x2: 1, y1: 0, y2: 0, gradientTransform: "rotate(2.372 -882.516 2470.562) scale(92.61108)", gradientUnits: "userSpaceOnUse" },
-            React.createElement("stop", { offset: 0, style: {
-                    stopColor: '#4fb38d',
-                    stopOpacity: 1,
-                } }),
-            React.createElement("stop", { offset: 1, style: {
-                    stopColor: '#97dec0',
-                    stopOpacity: 1,
-                } })),
-        React.createElement("linearGradient", { id: "cypress_svg__d", x1: 0, x2: 1, y1: 0, y2: 0, gradientTransform: "rotate(96.073 55.216 160.304) scale(112.63)", gradientUnits: "userSpaceOnUse" },
-            React.createElement("stop", { offset: 0, style: {
-                    stopColor: '#97dec0',
-                    stopOpacity: 1,
-                } }),
-            React.createElement("stop", { offset: 1, style: {
-                    stopColor: 'ivory',
-                    stopOpacity: 0,
-                } }),
-            React.createElement("stop", { offset: 1, style: {
-                    stopColor: 'ivory',
-                    stopOpacity: 0,
-                } }))))); };
+        } }))); };
 export default SvgCypress;
