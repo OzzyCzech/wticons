a53943d5984225027d62988b7e9c245696cf94dd
Roman Ožana <roman@ozana.cz>
2024-02-23 13:15:52 +0100

    Add index

--- packages/wticons-astro/build.mjs
+++ packages/wticons-astro/build.mjs
@@ -2,8 +2,8 @@ import { mkdir, readdir, readFile, writeFile } from 'node:fs/promises';
 import pascalcase from 'pascalcase';
 
 const icons = '../../icons/';
-
 await mkdir('dist', { recursive: true });
+await writeFile('dist/index.ts', '');
 
 for (const file of await readdir(icons, { encoding: 'utf8' })) {
   if (!file.endsWith('.svg')) continue; // skip non svg files
@@ -17,6 +17,9 @@ for (const file of await readdir(icons, { encoding: 'utf8' })) {
 
   const header = `---\nimport type { HTMLAttributes } from "astro/types";\nexport type Props = HTMLAttributes<'svg'>\n---\n\n`;
 
-
   await writeFile('dist/' + name + '.astro', header + svg.replace(`<svg `, `<svg {...Astro.props} `));
+
+  // create index.ts
+  const index = `export { default as ${name} } from './${name}.astro';\n`;
+  await writeFile('dist/index.ts', index, { flag: 'a' });
 }
