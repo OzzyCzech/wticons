2adcd66742c10522e03ee897ec3cdfed6ef59c3b
Roman Ožana <roman@ozana.cz>
2024-08-20 12:34:13 +0200

    Add prefix counter

--- svgo.config.mjs
+++ svgo.config.mjs
@@ -1,7 +1,16 @@
+let prefixCounter = 0;
+
 export default {
   //js2svg: { indent: 2, pretty: true },
   plugins: [
     'preset-default',
     'removeDimensions',
+    {
+      name: 'prefixIds',
+      params: {
+        delim: '',
+        prefix: () => `w${prefixCounter++}`,
+      },
+    },
   ],
 };
