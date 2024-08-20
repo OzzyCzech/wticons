21150dc0411c0eccae77c30fe177fcf3564170d6
Roman Ožana <roman@ozana.cz>
2024-08-20 13:49:44 +0200

    Links

--- update.js
+++ update.js
@@ -3,7 +3,7 @@ import { extname } from 'node:path';
 
 const icons = (await readdir('./icons')).sort().filter(file => extname(file) === '.svg');
 
-const list = icons.map(file => `* [<img src='icons/${file}' height='40' alt='${file}' />](icons/${file})`).join('\n');
+const list = icons.map(file => `[<img src='icons/${file}' height='40' alt='${file}' />](icons/${file})`).join('\n');
 const data = await readFile('./readme.md', 'utf8');
 
 const updated = data.replace(
