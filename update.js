32d3322327074c15f49af395ab38d54115551698
Roman Ožana <roman@ozana.cz>
2024-08-20 13:47:09 +0200

    Readme with links

---
+++ update.js
@@ -0,0 +1,14 @@
+import { readdir, readFile, writeFile } from 'node:fs/promises';
+import { extname } from 'node:path';
+
+const icons = (await readdir('./icons')).sort().filter(file => extname(file) === '.svg');
+
+const list = icons.map(file => `* [${file}](icons/${file})`).join('\n');
+const data = await readFile('./readme.md', 'utf8');
+
+const updated = data.replace(
+  /## Icons\n[\s\S]*?(?=\n##|$)/,
+  `## Icons\n\n${list}\n`,
+);
+
+await writeFile('./readme.md', updated, 'utf8');
