3e39e0b361602cb456e8d7626069949cbc2b861b
Roman Ožana <roman@ozana.cz>
2024-08-20 11:13:44 +0200

    Update Vitejs

--- packages/wticons-react/dist/Vitejs.js
+++ packages/wticons-react/dist/Vitejs.js
@@ -1,39 +1,13 @@
 import * as React from "react";
-const SvgVitejs = (props) => (React.createElement("svg", { xmlns: "http://www.w3.org/2000/svg", xmlSpace: "preserve", style: {
-        fillRule: "evenodd",
-        clipRule: "evenodd",
-        strokeLinejoin: "round",
-        strokeMiterlimit: 2,
-    }, viewBox: "0 0 256 257", ...props },
-    React.createElement("path", { d: "M255.153 37.938 134.897 252.976c-2.483 4.44-8.862 4.466-11.382.048L.874 37.958c-2.745-4.814 1.372-10.646 6.828-9.671l120.385 21.518a6.5 6.5 0 0 0 2.322-.004l117.867-21.483c5.438-.991 9.574 4.796 6.877 9.62", style: {
-            fill: "url(#vitejs_svg__a)",
-            fillRule: "nonzero",
-        } }),
-    React.createElement("path", { d: "M185.432.063 96.439 17.501a3.27 3.27 0 0 0-2.634 3.014l-5.474 92.456a3.268 3.268 0 0 0 3.997 3.378l24.777-5.718c2.318-.535 4.413 1.507 3.936 3.838l-7.361 36.047c-.495 2.426 1.782 4.5 4.151 3.781l15.304-4.65c2.372-.72 4.652 1.36 4.15 3.788l-11.698 56.621c-.732 3.542 3.979 5.473 5.943 2.437l1.313-2.028 72.516-144.719c1.215-2.424-.88-5.187-3.541-4.673l-25.504 4.922c-2.396.462-4.435-1.77-3.759-4.114l16.646-57.705c.677-2.349-1.369-4.583-3.769-4.113", style: {
-            fill: "url(#vitejs_svg__b)",
-            fillRule: "nonzero",
-        } }),
+const SvgVitejs = (props) => (React.createElement("svg", { xmlns: "http://www.w3.org/2000/svg", width: 256, height: 257, preserveAspectRatio: "xMidYMid", ...props },
     React.createElement("defs", null,
-        React.createElement("linearGradient", { id: "vitejs_svg__a", x1: 0, x2: 1, y1: 0, y2: 0, gradientTransform: "rotate(47.165 -53.273 20.473)scale(220.137)", gradientUnits: "userSpaceOnUse" },
-            React.createElement("stop", { offset: 0, style: {
-                    stopColor: "#41d1ff",
-                    stopOpacity: 1,
-                } }),
-            React.createElement("stop", { offset: 1, style: {
-                    stopColor: "#bd34fe",
-                    stopOpacity: 1,
-                } })),
-        React.createElement("linearGradient", { id: "vitejs_svg__b", x1: 0, x2: 1, y1: 0, y2: 0, gradientTransform: "rotate(87.488 67.115 75.141)scale(185.90458)", gradientUnits: "userSpaceOnUse" },
-            React.createElement("stop", { offset: 0, style: {
-                    stopColor: "#ffea83",
-                    stopOpacity: 1,
-                } }),
-            React.createElement("stop", { offset: 0.08, style: {
-                    stopColor: "#ffdd35",
-                    stopOpacity: 1,
-                } }),
-            React.createElement("stop", { offset: 1, style: {
-                    stopColor: "#ffa800",
-                    stopOpacity: 1,
-                } })))));
+        React.createElement("linearGradient", { id: "vitejs_svg__a", x1: "-.828%", x2: "57.636%", y1: "7.652%", y2: "78.411%" },
+            React.createElement("stop", { offset: "0%", stopColor: "#41D1FF" }),
+            React.createElement("stop", { offset: "100%", stopColor: "#BD34FE" })),
+        React.createElement("linearGradient", { id: "vitejs_svg__b", x1: "43.376%", x2: "50.316%", y1: "2.242%", y2: "89.03%" },
+            React.createElement("stop", { offset: "0%", stopColor: "#FFEA83" }),
+            React.createElement("stop", { offset: "8.333%", stopColor: "#FFDD35" }),
+            React.createElement("stop", { offset: "100%", stopColor: "#FFA800" }))),
+    React.createElement("path", { fill: "url(#vitejs_svg__a)", d: "M255.153 37.938 134.897 252.976c-2.483 4.44-8.862 4.466-11.382.048L.875 37.958c-2.746-4.814 1.371-10.646 6.827-9.67l120.385 21.517a6.5 6.5 0 0 0 2.322-.004l117.867-21.483c5.438-.991 9.574 4.796 6.877 9.62" }),
+    React.createElement("path", { fill: "url(#vitejs_svg__b)", d: "M185.432.063 96.44 17.501a3.27 3.27 0 0 0-2.634 3.014l-5.474 92.456a3.268 3.268 0 0 0 3.997 3.378l24.777-5.718c2.318-.535 4.413 1.507 3.936 3.838l-7.361 36.047c-.495 2.426 1.782 4.5 4.151 3.78l15.304-4.649c2.372-.72 4.652 1.36 4.15 3.788l-11.698 56.621c-.732 3.542 3.979 5.473 5.943 2.437l1.313-2.028 72.516-144.72c1.215-2.423-.88-5.186-3.54-4.672l-25.505 4.922c-2.396.462-4.435-1.77-3.759-4.114l16.646-57.705c.677-2.35-1.37-4.583-3.769-4.113" })));
 export default SvgVitejs;
